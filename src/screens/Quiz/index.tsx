499f581e8bfc0e05b5740e3d690787f927d504b4
TonySilva7 <tony.deve7oper@gmail.com>
2024-02-23 11:21:48 -0300

    feat: implement validate back routes

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, Text, View, BackHandler } from 'react-native';
 import { Audio } from 'expo-av';
+import * as Haptics from 'expo-haptics';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Animated, { 
@@ -132,7 +133,9 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }), 
       withTiming(0, undefined, (isFinished) => {
@@ -215,6 +218,12 @@ export function Quiz() {
     setQuiz(quizSelected);
     setIsLoading(false);
   }, []);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleStop)
+
+    return () => backHandler.remove();
+  },[])
 
   if (isLoading) {
     return <Loading />
